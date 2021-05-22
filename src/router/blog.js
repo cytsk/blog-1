01522336c85152a5be5e138da71ea01fd16592fc
cayongta <yongtao.cai@gmail.com>
2021-05-22 10:43:44 +0900

    revised msgs

--- src/router/blog.js
+++ src/router/blog.js
@@ -5,36 +5,36 @@ const handleBlogRouter = (req, res) => {
     if (method === 'GET' && req.path === '/api/blog/list') {
         return {
             msg: 'This is the interface of blog'
-        }
-    }
+        };
+    };
 
     // get the detail
     if (method === 'GET' && req.path === '/api/blog/detail') {
         return {
-            msg: 'This is the interface of blog'
-        }
-    }
+            msg: 'This is the interface of blog detail'
+        };
+    };
 
     // create a blog
     if (method === 'POST' && req.path === '/api/blog/new') {
         return {
             msg: 'This is the interface of creating blog'
-        }
-    }
+        };
+    };
 
     // update a blog
     if (method === 'POST' && req.path === '/api/blog/update') {
         return {
             msg: 'This is the interface of updating blog'
-        }
-    }
+        };
+    };
 
     // delete a blog
     if (method === 'POST' && req.path === '/api/blog/del') {
         return {
             msg: 'This is the interface of deleting blog'
-        }
-    }
+        };
+    };
 }
 
-module.exports = handleBlogRouter
+module.exports = handleBlogRouter;
